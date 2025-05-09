48098d1358263e2e38f1f9c6cd29245f6e28d085
Tonyrj3268 <chiacheng.lin0@gmail.com>
2025-05-09 20:26:20 +0800

    feat: Introduce ReceivablesRelatedParties model and integrate it into financial report processing

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from models.cash_equivalents import CashAndEquivalents, cash_equivalents_prompt
 from models.total_liabilities import TotalLiabilities, total_liabilities_prompt
 from models.prepayments import PrePayments, prepayments_prompt
 from models.total import FinancialReport, financial_report_prompt
+from models.receivables_related_parties import (
+    ReceivablesRelatedParties,
+    receivables_related_parties_prompt,
+)
 from pathlib import Path
 
 PDF_DIR = Path(__file__).parent.parent / "assets/pdfs"
@@ -13,10 +17,10 @@ MD_DIR = Path(__file__).parent.parent / "assets/markdowns"
 
 pdf_mapping = {
     "quartely-results-2024-zh_tcm27-94407.pdf": "file-KGXtvwDDkZ8wYCMRiAeRQg",  # 長榮航空
-    # "113Q4 華碩財報(個體).pdf": "file-FsNfKa6Ydbi2hRHKfW9TTw",  # 華碩
-    # "TSMC 2024Q4 Unconsolidated Financial Statements_C.pdf": "file-LQokuRBxkg2CEp3PZiFBMf",  # 台積電
+    "113Q4 華碩財報(個體).pdf": "file-FsNfKa6Ydbi2hRHKfW9TTw",  # 華碩
+    "TSMC 2024Q4 Unconsolidated Financial Statements_C.pdf": "file-LQokuRBxkg2CEp3PZiFBMf",  # 台積電
     # "20240314171909745560928_tc.pdf": "file-X269JoL59QfurudTY48adv",  # 中信金
-    # "fin_202503071324328842.pdf": "file-4YPtrJes7jpnUSRf7BVAx1",  # 統一
+    "fin_202503071324328842.pdf": "file-4YPtrJes7jpnUSRf7BVAx1",  # 統一
 }
 
 model_prompt_mapping = {
@@ -31,6 +35,10 @@ model_prompt_mapping = {
     "prepayments": {
         "prompt": prepayments_prompt,
         "model": PrePayments,
+    },
+    "receivables_related_parties": {
+        "prompt": receivables_related_parties_prompt,
+        "model": ReceivablesRelatedParties,
     },
     "financial_report": {
         "prompt": financial_report_prompt,
@@ -67,8 +75,8 @@ if __name__ == "__main__":
     results = {}
     for filename, file_id in pdf_mapping.items():
         pdf_path = PDF_DIR / filename
-        prompt = model_prompt_mapping["prepayments"]["prompt"]
-        model = model_prompt_mapping["prepayments"]["model"]
+        prompt = model_prompt_mapping["financial_report"]["prompt"]
+        model = model_prompt_mapping["financial_report"]["model"]
         res = process(pdf_path, prompt, model)
         results[filename] = res.model_dump()
     import json
--- src/models/total.py
+++ src/models/total.py
@@ -1,6 +1,10 @@
 from .cash_equivalents import CashAndEquivalents, cash_equivalents_prompt
 from .total_liabilities import TotalLiabilities, total_liabilities_prompt
-
+from .receivables_related_parties import (
+    ReceivablesRelatedParties,
+    receivables_related_parties_prompt,
+)
+from .prepayments import PrePayments, prepayments_prompt
 from pydantic import BaseModel, Field
 
 
@@ -9,6 +13,15 @@ class FinancialReport(BaseModel):
 
     cash_equivalents: CashAndEquivalents = Field(..., alias="現金及約當現金明細表")
     total_liabilities: TotalLiabilities = Field(..., alias="負債總額")  # 負債總額
+    receivables_related_parties: ReceivablesRelatedParties = Field(
+        ..., alias="應收帳款及應收票據明細表"
+    )
+    prepayments: PrePayments = Field(..., alias="預付款項明細表")
 
 
-financial_report_prompt = cash_equivalents_prompt + total_liabilities_prompt
+financial_report_prompt = (
+    cash_equivalents_prompt
+    + total_liabilities_prompt
+    + receivables_related_parties_prompt
+    + prepayments_prompt
+)
